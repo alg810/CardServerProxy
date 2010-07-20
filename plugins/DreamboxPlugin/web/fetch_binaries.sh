d1c25b98858545053def3b64ea0e4dd877e0eee9
merek <merek@96d545e8-fc7a-4ab8-b118-dd1d7516bec5>
2010-07-20 06:47:24 +0000

    fetch_binaries.sh gets now dropbear binaries from CSP Wiki

--- plugins/DreamboxPlugin/web/fetch_binaries.sh
+++ plugins/DreamboxPlugin/web/fetch_binaries.sh
@@ -1,40 +1,17 @@
 #!/bin/sh
 
-# FIXME
-echo "This Script should download dropbear 0.5x ppc and mips bins packed with upx ..."
-exit 1;
+BINARIES="dropbearmulti.ppc dropbearmulti.static.ppc dropbearmulti.mips dropbearmulti.static.mips"
 
-mkdir -p tmp
-rm -rf tmp/*
-cd tmp
-
-# Use an old binary to ensure no unmet dependencies on older 500-images
-echo "Retrieving dropbear 0.48 ppc binary..."
-wget -q http://downloads.pli-images.org/iolite/plugins/dropbear.tar.gz
-if [ -e dropbear.tar.gz ]; then
-  tar xzf dropbear.tar.gz
-  mv var/bin/dropbearmulti ../dropbearmulti.ppc
-  gzip ../dropbearmulti.ppc
-  mv ../dropbearmulti.ppc.gz ../binaries/
-  rm dropbear.tar.gz
-  rm -rf var
-  echo "Done"
-else
-  echo "Download failed: dropbear.tar.gz"
-  exit 1
-fi
-
-echo "Retrieving dropbear 0.48 mips binary..."
-wget -q http://downloads.pli-images.org/feeds/jade3/dm800/dropbear_0.48.1-r1_mipsel.ipk
-if [ -e dropbear_0.48.1-r1_mipsel.ipk ]; then
-  ar x dropbear_0.48.1-r1_mipsel.ipk
-  tar xf data.tar.gz
-  mv usr/sbin/dropbearmulti ../dropbearmulti.mips
-  gzip ../dropbearmulti.mips
-  mv ../dropbearmulti.mips.gz ../binaries/
-  rm -rf *
-  echo "Done"
-else
-  echo "Download failed: dropbear_0.48.1-r1_mipsel.ipk"
-  exit 1
-fi
+for BIN in $BINARIES
+do
+	echo "Retrieving $BIN ..."
+	wget -q http://streamboard.gmc.to:8011/raw-attachment/wiki/DropBearBins/$BIN
+	if [ -e $BIN ]; then
+		mv $BIN binaries/
+		chmod +x binaries/$BIN
+		echo "Done"
+	else
+		echo "Download failed: $BIN"
+		exit 1
+	fi
+done
