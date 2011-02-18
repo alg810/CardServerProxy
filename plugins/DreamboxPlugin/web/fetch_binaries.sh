e4fcdf5a3ab201cf7be2de813a25842836b00b62
merek <merek@96d545e8-fc7a-4ab8-b118-dd1d7516bec5>
2011-02-18 10:58:27 +0000

    Added new SH4 binaries to fetch_binaries.sh and changed the Wiki URL

--- plugins/DreamboxPlugin/web/fetch_binaries.sh
+++ plugins/DreamboxPlugin/web/fetch_binaries.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-BINARIES="dropbearmulti.ppc dropbearmulti.static.ppc dropbearmulti.04x.ppc dropbearmulti.mips dropbearmulti.static.mips"
+BINARIES="dropbearmulti.ppc dropbearmulti.static.ppc dropbearmulti.04x.ppc dropbearmulti.mips dropbearmulti.static.mips dropbearmulti.sh4 dropbearmulti.static.sh4"
 
 for BIN in $BINARIES
 do
 	echo "Retrieving $BIN ..."
-	wget -q http://streamboard.gmc.to:8011/raw-attachment/wiki/DropBearBins/$BIN
+	wget -q http://streamboard.gmc.to/CSP/raw-attachment/wiki/DropBearBins/$BIN
 	if [ -e $BIN ]; then
 		mv $BIN binaries/
 		chmod +x binaries/$BIN
