e7bdb3fea3b830d81d67439f14280d1267a5e33a
merek <merek@96d545e8-fc7a-4ab8-b118-dd1d7516bec5>
2011-03-28 10:13:44 +0000

    Updates for setup_mgcamd.sh in DreamBoxPlugin

--- plugins/DreamboxPlugin/web/setup_mgcamd.sh
+++ plugins/DreamboxPlugin/web/setup_mgcamd.sh
@@ -31,10 +31,122 @@ NCDIGNORE="/var/keys/ignore.list"
 NCDEXTPORT="12345"
 NCDKEEPALIVE="60"
 DESKEY="11 22 33 44 55 66 77 88 99 AA BB CC DD EE"
+# fallback
+FALLBACK="false"
+CSP_FALLBACK_HOST="fallback-host.domain.tld"
 ### end ###
 
-### init scripts for boxes ###
-GEMINI_CAM='CAMNAME="MgCamd 1.35a"
+### functions ###
+install_pkg() {
+
+	FEEDLIST=$1
+	PKG_NAME=$2
+	PKG_MANAGER=$3
+
+	if [ ! -e $FEEDLIST ]; then
+		echo "Updating feed list from server ..."
+		$PKG_MANAGER update
+		if [ $? != "0" ]; then
+			echo "Cannot get feed list from server ..."
+			return 1
+		fi
+	else
+		echo "Feedlist up-to-date ... "
+	fi
+
+	if [ $($PKG_MANAGER status $PKG_NAME | grep installed | wc -l) -ge 1 ]; then
+		echo "$PKG_NAME allready installed ..."
+		return 0
+	else
+		$PKG_MANAGER install $PKG_NAME
+		if [ $? != "0" ]; then
+			echo "Cannot install $PKG_NAME with $PKG_MANAGER ... "
+			return 1
+		else
+			echo "$PKG_NAME installed successfully ..."
+			return 0
+		fi	
+	fi
+}
+
+fetch_file() {
+
+	GET_FILE=$1
+	DEST_FILE=$2
+	EXECUTABLE=$3
+
+	wget -q -O /tmp/$GET_FILE http://$CSPHOST:$CSPPORT/open/binaries/$GET_FILE
+	if [ $? != "0" ]; then
+		echo "Cannot get $GET_FILE from server ..."
+		return 1
+	else
+		mv -f /tmp/$GET_FILE $DEST_FILE
+
+		if [ $EXECUTABLE = "1" ]; then
+			chmod +x $DEST_FILE
+		fi
+
+		echo "$GET_FILE successfully fetched and copied to $DEST_FILE ..."
+		return 0
+	fi
+
+}
+### end ###
+
+### main ###
+if [ $(echo $PARAMS | grep -i kill | wc -l) -ge 1 ]; then
+	echo "Killing newcamd.list ..."
+	rm -Rf $NCDLIST
+	echo "Rebooting system ..."
+	reboot
+	exit
+fi
+
+if [ $(echo $PARAMS | grep -i config | wc -l) -ge 1 ]; then
+	OLDIMAGE=$IMAGE
+	IMAGE="SkipInstall"
+fi
+
+# check if we have an image with GP3 plugin
+if [ $(echo $IMAGE | grep -i gp3 | wc -l) -ge 1 ]; then
+  IMAGE="Gemini"
+  GP3_PLUGIN_INSTALLED="1"
+fi
+
+case $IMAGE in
+	"Sportster Pro")
+		if [ -x /var/emu/mgcamd ]; then
+                        echo "Mgcamd allready installed ..."
+                        break
+                else
+			CPUARCH="ppc_old"
+			fetch_file "mgcamd.$CPUARCH" "/var/emu/mgcamd" "1"
+			if [ $? != "0" ]; then
+				break
+			else
+				# set default cam to mgcamd
+				echo "1.35a" > /var/emu/.mgcamd.txt
+				touch /var/etc/.mgcamd
+
+				# remove camd2 default
+				rm -Rf /var/etc/.camd2
+				echo "Mgcamd installed successfully on dbox2 with sportster image ..."
+			fi
+		fi
+		;;
+
+	"Gemini")
+		if [ -x /var/bin/mgcamd_1.35a ]; then
+			echo "Mgcamd allready installed ..."
+			break
+		else
+			fetch_file "mgcamd.$CPUARCH" "/var/bin/mgcamd_1.35a" "1"
+			if [ $? != "0" ]; then
+				break
+			else
+				# install cam script
+				echo '#!/bin/sh
+CAMNAME="MgCamd 1.35a"
 USERNAME="MgCamd 1.35a"
 START_TIME=4
 STOPP_TIME=2
@@ -64,194 +176,137 @@ case "$1" in
   ;;
 esac
 
-exit 0'
-### end ###
+exit 0' > /usr/script/mgcamd_1.35a_cam.sh
+				chmod +x /usr/script/mgcamd_1.35a_cam.sh
 
-### functions ###
+				# set default cam to mgcamd
+				if [ $GP3_PLUGIN_INSTALLED = "1" ]; then
+					sed -i "s/int=currCamd=.*/int=currCamd=4096/" /etc/enigma2/gemini_plugin.conf
+					echo "Mgcamd installed successfully on dreambox with installed GP3 Plugin ..."
+				else
+					case $ENIGMAV in
+						1)
+							sed -i "s/act_cam=.*/act_cam=4096/" /var/tuxbox/config/Gemini.conf
+							echo "Mgcamd installed successfully on dreambox with Gemini (E1) image ..."
+							;;
 
-install_opkg() {
-
-	FEEDLIST=$1
-	OPKG_NAME=$2
-
-	if [ ! -e /var/lib/opkg/$FEEDLIST ]; then
-		echo "Updating feed list from server ..."
-		opkg update
-		if [ $? != "0" ]; then
-			echo "Cannot get feed list from server ..."
-			return 1
-		fi
-	else
-		echo "Feedlist up-to-date ... "
-	fi
-
-	if [ $(opkg status $OPKG_NAME | grep installed | wc -l) -ge 1 ]; then
-		echo "Mgcamd pkg ($OPKG_NAME) allready installed ..."
-		return 0
-	else
-		opkg install $OPKG_NAME
-		if [ $? != "0" ]; then
-			echo "Cannot install pkg ($OPKG_NAME) with opkg ... "
-			return 1
-		else
-			echo "Pkg ($OPKG_NAME) installed successfully ..."
-			return 0
-		fi	
-	fi
-}
-
-### end ###
-
-### main ###
-if [ "$PARAMS" = "kill" ]; then
-	echo "Killing newcamd.list ..."
-	rm -Rf $NCDLIST
-	echo "Rebooting system ..."
-	reboot
-	exit
-fi
-
-if [ "$PARAMS" = "config" ]; then
-	IMAGE="SkipInstall"
-fi
-
-# check if we have an image with GP3 plugin, so we use plugin functions for cam installation
-if [ $(echo $IMAGE | grep GP3 | wc -l) -ge 1 ]; then
-  IMAGE="GeminiPlugin3"
-fi
-
-case $IMAGE in
-	"Sportster Pro")
-		if [ -x /var/emu/mgcamd ]; then
-                        echo "Mgcamd allready installed ..."
-                        break
-                else
-			CPUARCH="ppc_old"
-			wget -q -O /tmp/mgcamd http://$CSPHOST:$CSPPORT/open/binaries/mgcamd.$CPUARCH
-			if [ $? != "0" ]; then
-				echo "Cannot get mgcamd binary from server ..."
-			else
-				mv /tmp/mgcamd /var/emu/mgcamd
-				chmod +x /var/emu/mgcamd
-				echo "1.35a" > /var/emu/.mgcamd.txt
-				touch /var/etc/.mgcamd
-				rm -Rf /var/etc/.camd2
-				echo "Mgcamd installed successfully on dbox2 with sportster image ..."
+						2)
+							sed -i "s/int=used_camd=.*/int=used_camd=4096/" /var/etc/gemini2.conf
+							echo "Mgcamd installed successfully on dreambox with Gemini (E2) image ..."
+							;;
+					esac
+				fi
 			fi
 		fi
 		;;
 
-	"Gemini")
+	"Dreamelite")
 		if [ -x /var/bin/mgcamd_1.35a ]; then
 			echo "Mgcamd allready installed ..."
 			break
 		else
-
-			if [ $ENIGMAV -eq 1 ]; then
-				wget -q -O /var/bin/mgcamd_1.35a http://$CSPHOST:$CSPPORT/open/binaries/mgcamd.$CPUARCH
-				if [ $? != "0" ]; then
-					echo "Cannot get mgcamd binary from server ..."
-				else	
-					chmod +x /var/bin/mgcamd_1.35a
-
-					# install cam script
-					echo $GEMINI_CAM > /usr/script/mgcamd_1.35a_cam.sh
-					chmod +x /usr/script/mgcamd_1.35a_cam.sh
-	
-					# set default cam to mgcamd
-					cat /var/tuxbox/config/Gemini.conf | sed "s/act_cam=.*/act_cam=4096/" > /tmp/Gemini.conf.new
-					mv /var/tuxbox/config/Gemini.conf /var/tuxbox/config/Gemini.conf.bak
-					mv /tmp/Gemini.conf.new /var/tuxbox/config/Gemini.conf
-	
-					echo "Mgcamd installed successfully on dreambox with Gemini (E1) image ..."
-				fi
+			fetch_file "mgcamd.$CPUARCH" "/var/bin/mgcamd_1.35a" "1"
+			if [ $? != "0" ]; then
+				break
 			else
-				wget -q -O /var/bin/mgcamd_1.35a http://$CSPHOST:$CSPPORT/open/binaries/mgcamd.$CPUARCH
-				if [ $? != "0" ]; then
-					echo "Cannot get mgcamd binary from server ..."
-				else
-					chmod +x /var/bin/mgcamd_1.35a
-	
-					# install cam script
-					echo $GEMINI_CAM > /usr/script/mgcamd_1.35a_cam.sh
-					chmod +x /usr/script/mgcamd_1.35a_cam.sh
-	
-					# set default cam to mgcamd
-					cat /var/etc/gemini2.conf | sed "s/int=used_camd=.*/int=used_camd=4096/" > /tmp/gemini2.conf.new
-					mv /var/etc/gemini2.conf /var/etc/gemini2.conf.bak
-					mv /tmp/gemini2.conf.new /var/etc/gemini2.conf
-	
-					echo "Mgcamd installed successfully on dreambox with Gemini (E2) image ..."
-				fi
+				# install cam script
+				echo '#!/bin/sh
+#emuname=Mgcamd 1.35a
+#ecminfofile=ecm.info
+
+remove_tmp () {
+        rm -rf /tmp/ecm.info /tmp/pid.info /tmp/cardinfo /tmp/mg*
+}
+
+case "$1" in
+        start)
+        remove_tmp
+        /var/bin/mgcamd_1.35a &
+        sleep 3
+        ;;
+        stop)
+        killall -9 mgcamd_1.35a
+        remove_tmp
+        sleep 2
+        ;;
+        *)
+        $0 stop
+        exit 1
+        ;;
+esac
+
+exit 0' > /usr/script/mgcamd_135a_em.sh
+				chmod +x /usr/script/mgcamd_135a_em.sh
+
+				# set default cam to mgcamd
+				echo "mgcamd_135a" > emuactive
+				echo "Mgcamd installed successfully on dreambox with Dreamelite image ..."
 			fi
 		fi
 		;;
 
 	"Newnigma2")
-		install_opkg "softcammipsel" "newnigma2-camd-mgcamd1-35a"
+		install_pkg "/var/lib/opkg/softcammipsel" "newnigma2-camd-mgcamd1-35a" "opkg"
 		if [ $? != "0" ]; then
-			break;
+			break
+		else
+			# set default cam to mgcamd
+			ln -sf /usr/script/mgcamd1.35.emu /etc/rc3.d/S98emustart
+			echo "Mgcamd installed successfully on dreambox with Newnigma2 image ..."
 		fi
 		;;
 
-	"iCVS")
-		# TODO 
-		# - automatically GP3 Plugin installation
-		# - reinstall CSP agent to get rid of new Image Guess
-		# - requeue an setup_mgcamd task
+	"AAF")
+		install_pkg "/usr/lib/ipkg/cross" "enigma2-plugin-emus-mgcamd.v1.35" "ipkg"
+		if [ $? != "0" ]; then
+			break
+		else
+			sed -i 's/emu=.*/emu=\"mgcamd_1.35\ \&\"/' /etc/init.d/autostart/start-config
+			echo "Mgcamd installed successfully on ufs912 with AAF image ..."
+		fi
 		;;
 
-	"GeminiPlugin3")
-		install_opkg "gemini-mipsel" "gp-cam-mgcamd-1.35a"
+	"VTi")
+		install_pkg "/var/lib/ipkg/VTI" "enigma2-plugin-cams-mgcamd.1.35" "ipkg"
 		if [ $? != "0" ]; then
-			break;
+			break
 		else
-			# set default cam to mgcamd
-			echo "Set Mgcamd as default cam ..."
-			cat /etc/enigma2/gemini_plugin.conf | sed "s/int=currCamd=.*/int=currCamd=4096/" > /tmp/gemini_plugin.conf.new
-			mv /etc/enigma2/gemini_plugin.conf /etc/enigma2/gemini_plugin.conf.bak
-			mv /tmp/gemini_plugin.conf.new /etc/enigma2/gemini_plugin.conf
-			rm -Rf /etc/enigma2/gemini_plugin.conf.bak
+			ln -sf /usr/script/mgcamd_1.35_cam.sh /etc/init.d/current_cam.sh
+			echo "Mgcamd installed successfully on Vu+ with VTi image ..."
 		fi
 		;;
 
 	"SkipInstall")
 		echo "Config only parameter set ... will skip installation ..."
+		IMAGE=$OLDIMAGE
 		;;
 
 	*)
-		echo "No supported image found ... generating only config ..."
+		echo "No supported image found ... config only ..."
 		;;
 esac
 
 # generate new newcamd.list
 echo "Generate newcamd.list ..."
-echo -e "CWS_KEEPALIVE = $NCDKEEPALIVE\nCWS = $CSPHOST $NCDEXTPORT $USERNAME $PASSWORD $DESKEY wan server1" > $NCDLIST
+if [ "$FALLBACK" = "true" ]; then
+  echo -e "CWS_KEEPALIVE = $NCDKEEPALIVE\nCWS = $CSPHOST $NCDEXTPORT $USERNAME $PASSWORD $DESKEY wan server1\nCWS = $CSP_FALLBACK_HOST $NCDEXTPORT $USERNAME $PASSWORD $DESKEY wan fallback1" > $NCDLIST
+else
+  echo -e "CWS_KEEPALIVE = $NCDKEEPALIVE\nCWS = $CSPHOST $NCDEXTPORT $USERNAME $PASSWORD $DESKEY wan server1" > $NCDLIST
+fi
 
 # generate ignore.list
 echo "Generate ignore.list ..."
 echo -e "X: { 09 C4 }\nX: { 09 C7 }\nX: { 09 AF }\nX: { 18 11 }\nX: { 18 15 }\nX: { 18 36 }" > $NCDIGNORE
 
 # generate priority.list
-#echo "Generate priority.list ..."
-#echo -e "X: { 0B 00 }\nX: { 17 20 }\nX: { 17 22 }\nX: { 18 30 }" > $NCDPRIO
+echo "Generate priority.list ..."
+echo -e "X: { 0B 00 }\nX: { 17 20 }\nX: { 17 22 }\nX: { 18 30 }" > $NCDPRIO
 
 # get SoftCam.Key and AutoRoll.Key from Server
 KEYFILES="SoftCam.Key AutoRoll.Key"
 for KEYFILE in $KEYFILES
 do
-	echo "Getting $KEYFILE from Server ..."
-	wget -q -O /tmp/$KEYFILE http://$CSPHOST:$CSPPORT/open/binaries/$KEYFILE
-	if [ $? != "0" ]; then
-		echo "Cannot get $KEYFILE from server ..."
-	else
-		if [ -e /var/keys/$KEYFILE ]; then
-			echo "Found old $KEYFILE ... deleting ..."
-			rm -Rf /var/keys/$KEYFILE
-		fi
-		mv /tmp/$KEYFILE /var/keys/$KEYFILE
-		echo "$KEYFILE installed successfully ... "
-	fi
+	fetch_file "$KEYFILE" "/var/keys/$KEYFILE" "0"
 done
 
 # generate mg_cfg
@@ -268,9 +323,9 @@ U: { 01 } 0x12c0
 T: { 00 }
 G: { 01 }
 N: { 07 } 5 30
-K: { 10 }
-Q: { 600 }
-P: { 00 } " >> $NCDCFG
+K: { 07 }
+Q: { 120 }
+P: { 00 }" >> $NCDCFG
 
 case $ENIGMAV in
 	1)
@@ -282,7 +337,7 @@ case $ENIGMAV in
 		;;
 		
 	*)
-		echo "O: { 00 } root dreambox" >> $NCDCFG
+		echo "O: { 00 } root password" >> $NCDCFG
 		;;
 esac
 
@@ -291,9 +346,28 @@ L: { 02 } 172.16.1.1 28007 /tmp/mgcamd.log
 E: { 15 }
 H: { 07 }
 R: { 04 }
-D: { 00 }
-B: { 00 }
-F: { 00 }" >> $NCDCFG
+D: { 00 }" >> $NCDCFG
+
+case $BOXTYPE in
+	ufs912)
+		if [ "$IMAGE" = "AAF" ]; then
+			echo "B: { 11 }" >> $NCDCFG
+		else
+			echo "B: { 08 }" >> $NCDCFG
+		fi
+		;;
+
+	*)
+		echo "B: { 00 }" >> $NCDCFG
+		;;
+esac
+
+echo "F: { 00 }" >> $NCDCFG
+
+# reboot box if requested
+if [ $(echo $PARAMS | grep -i reboot | wc -l) -ge 1 ]; then
+	reboot
+fi
 
 # flush buffers
 sync
