7bea36277fbba58388aee3509a86010f1df5b43e
merek <merek@96d545e8-fc7a-4ab8-b118-dd1d7516bec5>
2011-04-13 14:49:31 +0000

    Make enigma_osd.sh and enigma_zap.sh more generic

---
+++ plugins/DreamboxPlugin/web/open/scripts/osd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. /var/etc/cspagent.conf
+
+MSG="{PARAMS}"
+
+if [ -z "$MSG" ]; then
+  echo "No message specified."
+  exit
+else
+  MSG="Message from $CSPHOST:%0A$MSG"
+  MSG=$(echo "$MSG" | sed 's/[ ]/%20/g')
+fi
+
+case $OSDTYPE in
+  "enigma")
+    if [ $OSDVER -eq 1 ]; then
+      wget -q -O - "http://$OSDUSER:$OSDPASS@127.0.0.1/cgi-bin/xmessage?caption=message&body=$MSG&timeout=10"
+    else
+      wget -q -O - "http://$OSDUSER:$OSDPASS@127.0.0.1/web/message?text=$MSG&type=1&timeout=10"
+    fi
+    ;;
+
+  "neutrino")
+    ;;
+esac
