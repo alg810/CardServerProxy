f3174ca34eaca763f5facd5fcfe85e454adaf114
merek <merek@96d545e8-fc7a-4ab8-b118-dd1d7516bec5>
2011-06-23 10:33:57 +0000

    Added Busybox binary for sh4 to fetch_binaries.sh and Trac Wiki

--- plugins/DreamboxPlugin/web/fetch_binaries.sh
+++ plugins/DreamboxPlugin/web/fetch_binaries.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-BINARIES="dropbearmulti.ppc dropbearmulti.static.ppc dropbearmulti.04x.ppc dropbearmulti.mips dropbearmulti.static.mips dropbearmulti.sh4 dropbearmulti.static.sh4"
+DROPBEARBINS="dropbearmulti.ppc dropbearmulti.static.ppc dropbearmulti.04x.ppc dropbearmulti.mips dropbearmulti.static.mips dropbearmulti.sh4 dropbearmulti.static.sh4"
+BUSYBOXBINS="busybox.sh4"
 
-for BIN in $BINARIES
-do
-	echo "Retrieving $BIN ..."
-	wget -q http://streamboard.gmc.to/CSP/raw-attachment/wiki/DropBearBins/$BIN
+get_bin_from_trac() {
+
+	BIN=$1
+	TYPE=$2
+
+	echo -n "Retrieving $BIN ... "
+	wget -q http://streamboard.gmc.to/CSP/raw-attachment/wiki/$TYPE/$BIN
 	if [ -e $BIN ]; then
 		mv $BIN binaries/
 		chmod +x binaries/$BIN
@@ -14,4 +18,14 @@ do
 		echo "Download failed: $BIN"
 		exit 1
 	fi
+}
+
+for BIN in $DROPBEARBINS
+do
+	get_bin_from_trac $BIN DropBearBins
+done
+
+for BIN in $BUSYBOXBINS
+do
+	get_bin_from_trac $BIN BusyBoxBins
 done
