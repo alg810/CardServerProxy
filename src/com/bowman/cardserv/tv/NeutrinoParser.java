2f1fa136803f8848f05c9fb043f32f395e1b817c
lattjo <lattjo@96d545e8-fc7a-4ab8-b118-dd1d7516bec5>
2012-07-30 15:15:42 +0000

    NeutrinoParser: Added support for zapit api version 3, used by NeutrinoHD. Also added support for cable.

--- src/com/bowman/cardserv/tv/NeutrinoParser.java
+++ src/com/bowman/cardserv/tv/NeutrinoParser.java
@@ -35,30 +35,66 @@ public class NeutrinoParser {
     Map services = new HashMap();
 
     XMLConfig sat, tp, chan; TvService service; int[] tokens = new int[6]; int onid, tid, i = 1;
-    for(Enumeration e = xml.getMultipleSubConfigs("sat"); e.hasMoreElements(); ) {
+
+    String api = xml.getString("api");
+
+    String transponderStr, idStr, onidStr, channelStr, serviceIdStr, serviceTypeStr, serviceNameStr;
+
+    if(api != null && Integer.parseInt(api) == 3) {
+      // zapit api version 3 used by Neutrino HD
+      transponderStr = "TS";
+      idStr = "id";
+      onidStr = "on";
+      channelStr = "S";
+      serviceIdStr = "i";
+      serviceTypeStr = "t";
+      serviceNameStr = "n";
+    }
+    else {
+      transponderStr = "transponder";
+      idStr = "id";
+      onidStr = "onid";
+      channelStr = "channel";
+      serviceIdStr = "service_id";
+      serviceTypeStr = "service_type";
+      serviceNameStr = "name";
+    }
+
+    sat = xml.getSubConfig("sat");
+
+    Enumeration e;
+
+    // check if satelite or cable config
+    if(sat != null) {
+      e = xml.getMultipleSubConfigs("sat");
+    } else {
+      e = xml.getMultipleSubConfigs("cable");
+    }
+
+    while(e.hasMoreElements()) {
       sat = (XMLConfig)e.nextElement();
 
       if(filterSet == null || matchesFilter(sat.getString("name"), filterSet)) {
 
-        for(Enumeration n = sat.getMultipleSubConfigs("transponder"); n.hasMoreElements(); ) {
+        for(Enumeration n = sat.getMultipleSubConfigs(transponderStr); n.hasMoreElements(); ) {
           tp = (XMLConfig)n.nextElement();
-          tid = Integer.parseInt(tp.getString("id"), 16);
-          onid = Integer.parseInt(tp.getString("onid"), 16);
+          tid = Integer.parseInt(tp.getString(idStr), 16);
+          onid = Integer.parseInt(tp.getString(onidStr), 16);
 
           if(filterSet != null || networkId == onid) {
             // manual filter match or matching onid -> proceed with services
 
-            for(Enumeration m = tp.getMultipleSubConfigs("channel"); m.hasMoreElements(); ) {
+            for(Enumeration m = tp.getMultipleSubConfigs(channelStr); m.hasMoreElements(); ) {
               chan = (XMLConfig)m.nextElement();
-              tokens[0] = Integer.parseInt(chan.getString("service_id"), 16);
+              tokens[0] = Integer.parseInt(chan.getString(serviceIdStr), 16);
               tokens[1] = 0;
               tokens[2] = tid;
               tokens[3] = filterSet==null?onid:networkId; // if a filter was set the intent was to override, so use id from profile
-              tokens[4] = Integer.parseInt(chan.getString("service_type"), 16);
+              tokens[4] = Integer.parseInt(chan.getString(serviceTypeStr), 16);
               tokens[5] = i++;
 
               service = new TvService(tokens, profile);
-              service.setName(chan.getString("name"));
+              service.setName(chan.getString(serviceNameStr));
 
               addService(services, service);
             }
@@ -66,6 +102,7 @@ public class NeutrinoParser {
         }
       }
     }
+
     return services;
   }
 
